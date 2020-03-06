b8b621110993fb103780b5182dc7f9a0bd7d2849
Luiz Claudio <luizcns18@gmail.com>
2020-03-06 17:56:24 -0300

    Show album list

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'js-spotify-wrapper-lib';
 
 const spotify = new SpotifyWrapper({
-  token: 'YOUR_TOKEN_HERE',
+  token: 'BQAlRfAqQyfAX1tyFNyiZbDnhB1tWZ1PuRHi7wlZr3zBGnU1opbsRqw3vn4ct4P_DZPWFr6tRb2DwQZh_GXSkwORop0V-qfjxgldJHTf4Q7fDdnu15Af4ypI8U1Gnok0sHdQTY0',
 });
 
 export default spotify;
