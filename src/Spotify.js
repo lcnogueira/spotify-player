23fa39621ea805eb8b69cc7620813dbcf3b3926a
Luiz Claudio <luizcns18@gmail.com>
2020-03-09 11:47:53 -0300

    Add AlbumTracks and its tests

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'js-spotify-wrapper-lib';
 
 const spotify = new SpotifyWrapper({
-  token: 'BQCvSlC4Wx2Q8PxyxxPsHbk0u4uXyi5mBXDP8Zt23UhzLjNCqS2G-GE3It-4a5FKWK6jDYLzpvX9IWerQtDPCJjtux1I-c_6nt6cBDrMLcn7xKXs-Xrnriy3pdvfcu_BGjzHxtk',
+  token: 'BQCMKvs60c1eZBCywwCouOKBsDUldpWPmKxdh9YSu9VuONXm5OqDi81J8VC7_isG8hKJle4eN9Zjw4XTn2Zb2dxokl_q1aoOnjwWWujGEJ8A0mGPLrou5Eyb4F3822yAOeMKiHo',
 });
 
 export default spotify;
