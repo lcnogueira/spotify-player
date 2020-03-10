be854805e16be9593a8a895571d6450002d9aa64
Luiz Claudio <luizcns18@gmail.com>
2020-03-10 01:04:33 -0300

    Add SelectAlbumTrigger

---
+++ src/SelectAlbumTrigger.js
@@ -0,0 +1,20 @@
+import spotify from './Spotify';
+import renderAlbumInfo from './AlbumInfo';
+import renderAlbumTracks from './AlbumTracks';
+
+const albumsList = document.getElementById('album-list');
+const albumInfo = document.getElementById('album-info');
+const albumTracks = document.getElementById('album-tracks');
+
+function makeRequest(albumId) {
+  spotify.album.getAlbum(albumId)
+    .then((data) => renderAlbumInfo(data, albumInfo))
+    .then((data) => renderAlbumTracks(data.tracks.items, albumTracks));
+}
+
+export default function selectAlbumTrigger() {
+  albumsList.addEventListener('click', (e) => {
+    const { target } = e;
+    makeRequest(target.getAttribute('data-album-id'));
+  });
+}
--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'js-spotify-wrapper-lib';
 
 const spotify = new SpotifyWrapper({
-  token: 'BQDFwmwy90R1X5p04l94ChTKbZOWqqbjjMj8vOxrXcDLXiipKJTNiTaeMiyjTbF18Ps7jAqKR8pEvK8GYU-BrgfjtToP5_5NbR3DLM1ZK5so1j6__e4iOoEZJ04k56ngI-Jr0G8',
+  token: 'BQCev8RI-XOh3nJPhOjuVa1zkrPCUqr3ZRuRy1b51LvOzrQQBl_yoawpkWCtDg15dybK3drgMfhNqNqlCDqn_u1_quqEQJYZHXAS_wKPXx2J37aH4CByKumRewLbyg8YKquJiIU',
 });
 
 export default spotify;
