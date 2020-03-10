d459b7b1161de885d5da812dd8a34567d481de01
Luiz Claudio <luizcns18@gmail.com>
2020-03-10 01:20:32 -0300

    Add PlayListTrigger.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 
 import searchEnterTrigger from './SearchTrigger';
 import selectAlbumTrigger from './SelectAlbumTrigger';
+import playlistTrigger from './PlayListTrigger';
+
 
 searchEnterTrigger();
 selectAlbumTrigger();
+playlistTrigger();
